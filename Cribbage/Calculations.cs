d702cf85d7ed97e38f7aeacde9b6a9d746a8642a
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-05-15 11:57:47 +0200

    Calculations methods incorporated in Combination object

--- Cribbage/Calculations.cs
+++ Cribbage/Calculations.cs
@@ -5,14 +5,14 @@ namespace Cribbage
 {
     public static class Calculations
     {
-        public static int GetFifteens(this Card[] cards)
+        public static IEnumerable<int>GetFifteens(this Card[] cards)
         {
             // sort array
             // start from largest card
             // skip through combinations that are more than 15
             throw new NotImplementedException();
         }
-        public static int GetPairs(this Card[] cards)
+        public static IEnumerable<int> GetPairs(this Card[] cards)
         {
             int output = 0;
             for (int i = 0; i < cards.Length; i++) {
@@ -26,7 +26,7 @@ namespace Cribbage
             }
             return output;
         }
-        public static int GetRuns(this Card[] cards)
+        public static IEnumerable<int> GetRuns(this Card[] cards)
         {
             int output = 0;
             // get trimmed cards
@@ -35,9 +35,9 @@ namespace Cribbage
             // else, uhhhh... a bit more complicated
             throw new NotImplementedException();
         }
-        public static int GetHat(this Card[] cards)
+        public static IEnumerable<int> GetHat(this IEnumerable<Card> cards)
         {
-            for (int i = 0; i < 4; i++) 
+            for (int i = 0; i < cards.Count(); i++) 
             {
                 if (cards[i].Rank == CardValue.JACK && cards[i].Suit == cards[5].Suit) 
                 {
@@ -46,68 +46,16 @@ namespace Cribbage
             }
             return 0;
         }
-        public static int GetFlush(this Card[] cards)
+        public static IEnumerable<int> GetFlush(this IEnumerable<Card> cards)
         {
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < cards.Count(); i++)
             {
-                if (cards[i].Suit == cards[0].Suit)
+                if (cards.ElementAt(i).Suit != cards.ElementAt(0).Suit)
                 {
                     return 0;
                 }
             }
             return 1;
-        }
-        private static Card[] Sort(this Card[] cards) 
-        {
-            Card[] orderedCards = new Card[cards.Length];
-            for (int i = 0; i < cards.Length; i++)
-            {
-                for (int j = 0; j < orderedCards.Length; j++)
-                {
-                    if (orderedCards[j] == null)
-                    {
-                        orderedCards[0] = cards[i];
-                    }
-                    else if (cards[i].Rank.Rank < orderedCards[j].Rank.Rank)
-                    {
-                        orderedCards[j + 1] = orderedCards[j];
-                        orderedCards[j] = cards[i];
-                    }
-                }
-
-            }
-            return orderedCards;
-        }
-        private static Card[] Trim(this Card[] cards) 
-        {
-            Card[] orderedCards = cards.Sort();
-            List<Card> trimmedCards = new List<Card>();
-            int currentSequence = 0;
-            int sequenceStart = -1;
-            for (int i = 1; i < orderedCards.Length; i++)
-            {
-                if (orderedCards[i].Rank.Rank == orderedCards[i - 1].Rank.Rank + 1)
-                {
-                    if (sequenceStart < 0) 
-                    {
-                        sequenceStart = i - 1;
-                    }
-                    currentSequence++;
-                }
-                else if (orderedCards[i].Rank.Rank != orderedCards[i - 1].Rank.Rank || 
-                    i == orderedCards.Length - 1) 
-                {
-                    if (currentSequence >= 2) 
-                    {
-                        for (int j = sequenceStart; j < i; j++) 
-                        {
-                            trimmedCards.Add(trimmedCards[j]);
-                        }
-                    }
-                    currentSequence = 0;
-                }
-            }
-            return trimmedCards.ToArray();
         }
     }
 }
--- Cribbage/Combination.cs
+++ Cribbage/Combination.cs
@@ -1,9 +1,149 @@
-﻿namespace Cribbage
+﻿using System.Net.Sockets;
+
+namespace Cribbage
 {
     public class Combination
     {
-        public Card[] Hand { get; set; } = new Card[4];
-        public Card[] Discard { get; set; } = new Card[2];
-        public int ExpectedValue { get; set; }
+        /// <summary>
+        /// There are 46 possible draw cards
+        /// </summary>
+        private const int NUM_DRAW_CARDS = 46;
+        private readonly Card[] Hand;
+        private readonly Card[] Discard;
+        public Combination(Card[] hand, Card[] discard) 
+        {
+            Hand = SortCards(hand);
+            Discard = SortCards(discard);
+            getReplicates();
+            getFlush();
+            getHat();
+            GetRuns();
+        }
+        public int ValueInHand => AggregateValue(0);
+        /// <summary>
+        /// The expected value added by the draw card.
+        /// 
+        /// The expected value is the sum of the value added by each possible draw, divided by the number of possible draws.
+        /// </summary>
+        public double ExpectedDrawValue => (double)AggregateValue(1) / NUM_DRAW_CARDS;
+        private void getReplicates()
+        {
+            int replicates = 0;
+            for(int i = 1; i < this.Hand.Length; i++) 
+            {
+                if (this.Hand[i].Rank.Rank == this.Hand[i - 1].Rank.Rank)
+                {
+                    replicates += 1;
+                }
+                else 
+                {
+                    switch (replicates) 
+                    {
+                        case 1:
+                            this.Pairs.Add(this.Hand[i - 1].Rank.Rank);
+                            break;
+                        case 2:
+                            this.Trips.Add(this.Hand[i - 1].Rank.Rank);
+                            break;
+                        case 3:
+                            this.FourOfAKind = this.Hand[i - 1].Rank.Rank;
+                            break;
+                        default: break;
+                    }
+                    replicates = 0;
+                }
+            }
+        }
+        private void getFlush() 
+        {
+            this.Flush = true;
+            for (int i = 1; i < this.Hand.Length; i++) 
+            {
+                this.Flush = this.Hand[i].Suit == this.Hand[0].Suit && this.Flush;
+                if (!this.Flush) break;
+            }
+        }
+        private void getHat() 
+        {
+            for (int i = 0; i < this.Hand.Length; i++) 
+            {
+                if (this.Hand[i].Rank == CardValue.JACK) 
+                {
+                    this.Hat.Add(this.Hand[i].Suit, 0);
+                }
+            }
+            for (int i = 0; i < this.Hand.Length; i++)
+            {
+                Suit curSuit = this.Hand[i].Suit;
+                if (this.Hand[i].Rank != CardValue.JACK && this.Hat.Keys.Contains(curSuit))
+                {
+                    this.Hat[curSuit]++;
+                }
+            }
+        }
+        private void GetRuns() 
+        {
+            int sequenceStart = 0;
+            for(int i = 1; i < this.Hand.Length; i++) 
+            {
+                int sequenceEnd = this.Hand[i].Rank.Rank;
+                if (sequenceEnd == this.Hand[i - 1].Rank.Rank + 1)
+                {
+                    sequenceStart = this.Hand[i - 1].Rank.Rank;
+                }
+                else 
+                {
+                    if (sequenceEnd - sequenceStart > 2) 
+                    {
+                        this.Runs.Add(new int[] { sequenceStart, sequenceEnd });
+                    }
+                }
+            }
+        }
+        private List<int> Pairs { get; set; } = new();
+        private List<int> Trips { get; set; } = new();
+        private int FourOfAKind { get; set; }
+        private bool Flush { get; set; }
+        private List<int[]> Runs { get; set; }
+        private Dictionary<Suit, int> Hat { get; set; } = new();
+
+
+
+
+        // TODO: replace AggregateValue two separate methods and put them in respective getters.
+        private int AggregateValue(int index) 
+        {
+            throw new NotImplementedException();
+        }
+
+
+        
+
+        private Card[] SortCards(Card[] cards) 
+        {
+            Card[] output = new Card[cards.Length];
+            for (int i = 0; i < cards.Length; i++) 
+            {
+                Card shunt = cards[i];
+                for (int j = 0; i < output.Length; j++) 
+                {
+                    if (output[j] == null)
+                    {
+                        output[j] = shunt;
+                    }
+                    // if shunt card is less than current occupant of output[j], then swap cards
+                    else if (shunt.Rank.CompareTo(output[j].Rank) < 0) 
+                    {
+                        Card hold = shunt;
+                        shunt = output[j];
+                        output[j] = hold;
+                    }
+                }
+            }
+            return output;
+        }
+
+
+        // TODO: calculate fifteens with tree structure - use composite pattern
     }
 }

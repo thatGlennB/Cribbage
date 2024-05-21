88997c8a40f5d338e1b60bf3f19a8ba0d6bb720a
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-05-21 10:51:42 +0200

    added TODOs to Program

--- Cribbage/Program.cs
+++ Cribbage/Program.cs
@@ -2,3 +2,28 @@
 
 Console.WriteLine("Hello, World!");
 
+/* TODO: "selection" object containing:
+ *  - register object
+ *  - roots for each mode (3 in total)
+ *  - method to check for flush
+ *  - method to check for hat
+ *  - method that loops draw card and calculates score
+ *  - score register
+ */
+
+/* TODO: "Hand" class, containing six cards and 6C4 = 15 "Selection" objects
+ *  - stringbuilder containing output of calculations:
+ *      * score for every hand
+ *      * ordered in descending total value
+ *  - ability to access registers of each "selection" object if more granular output is needed
+ */
+
+/* TODO: "Menu" class, that mediates between user and program
+ *  - presents options to user and registers user input:
+ *      * records cards in user's hand
+ *      * outputs calculation to user
+ *      * gives user the option to explore more granular statistics
+ */
+
+// TODO: organize project in terms of MVC
+// TODO: create more appealing view (blazor? api?)

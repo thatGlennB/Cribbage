ad260ee5e2224cb85ea684cc369be70d4f7591e8
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-05-21 20:50:34 +0200

    Implemented Singleton structure in CardsSingleton

---
+++ Cribbage/CardsSingleton.cs
@@ -0,0 +1,40 @@
+﻿namespace Cribbage
+{
+    public sealed class CardsSingleton
+    {
+        private CardsSingleton() { }
+        private static readonly Lazy<CardsSingleton> _s = new Lazy<CardsSingleton>(() => new CardsSingleton());
+        public static CardsSingleton Instance
+        {
+            get => _s.Value;
+        }
+
+        public ISet<Card> Cards = new HashSet<Card>();
+
+        public void Add(Card card)
+        {
+            if (!Cards.Contains(card))
+            {
+                Cards.Add(card);
+            }
+
+        }
+
+        public void Add(ISet<Card> cards)
+        {
+            foreach (Card card in cards)
+            {
+                Add(card);
+            }
+        }
+
+        public void Remove(Card card)
+        {
+            if (Cards.Contains(card))
+            {
+                Cards.Remove(card);
+            }
+        }
+
+    }
+}

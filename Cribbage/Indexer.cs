8b9be5314794b50e878e546ef20f9af748934155
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-05-22 10:33:52 +0200

    Created indexer singleton to provide unique indices

---
+++ Cribbage/Indexer.cs
@@ -0,0 +1,15 @@
+﻿namespace Cribbage
+{
+    internal class Indexer
+    {
+        #region Singleton pattern
+        private Indexer() {}
+        private static readonly Lazy<Indexer> _s = new Lazy<Indexer>(() => new Indexer());
+        protected int _count { get; set; } = 0;
+        #endregion
+        public static int Next
+        {
+            get => ++_s.Value._count;
+        }
+    }
+}
